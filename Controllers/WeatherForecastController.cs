3428ff7e3786fa185aafd48f7b79f7f91e24d23b
Pankaju <Pankaju@srvsoftwaresolutions.com>
2020-09-25 18:49:27 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> myControllerAction()
+        {
+            //var t1 = service.getdata1Async();
+            //var t2 = service.getdata2Async();
+            //var t3 = service.getdata3Async();
+            //await Task.WhenAll(t1, t2, t3);
+
+            //var data = new returnObject
+            //{
+            //    d1 = await t1,
+            //    d2 = await t2,
+            //    d3 = await t3
+            //};
+            var data = "Hello";
+
+            return Ok(data);
+        }
+    }
+}
